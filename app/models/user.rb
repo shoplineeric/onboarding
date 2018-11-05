285a24f1fff9786620811987e33b9db0464f48fb
Eric Lu <eric.lu@shoplineapp.com>
2018-11-05 16:30:35 +0800

    feat(user-model-validation): add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	validates :first_name, :last_name, presence: true
+	validates :age, numericality: { greater_than: 0 }
+	validates :gender, inclusion: { in: %w(male female other) }
 end
