107e3647b290793f8f41736eda7c7f9ec6774457
Eric Lu <eric.lu@shoplineapp.com>
2018-11-05 21:13:02 +0800

    feat(use-mongodb): add address hash to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User
 	field :last_name, type: String
 	field :age, type: Integer
 	field :gender, type: String
+	field :address, type: Hash, default:{ :country => '', :address_1 => '', :address_2 => ''}
 
 	validates :first_name, :last_name, presence: true
 	validates :age, numericality: { greater_than: 0 }
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :first_name, :last_name, :created_at, :updated_at, :age, :gender
+json.extract! user, :id, :first_name, :last_name, :created_at, :updated_at, :age, :gender, :address
 json.url user_url(user, format: :json)
